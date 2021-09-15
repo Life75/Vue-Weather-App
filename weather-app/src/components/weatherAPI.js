7e9c4a455a2520c2ee9c3503631db99c18b1f9b1
Life75 <austynwashington@yahoo.com>
2021-09-15 15:09:29 -0400

    working on pulling the data

--- weather-app/src/components/weatherAPI.js
+++ weather-app/src/components/weatherAPI.js
@@ -1,37 +1,6 @@
-var LOCATION_API_LINK = "https://us1.locationiq.com/v1/reverse.php?key=";
-var LOCATION_KEY = "pk.3af66cb0b58e34d555ab8b604c40db2c";
-function getLocation() {
-    var cityName;
-    var options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-    function error(err) {
-        console.warn("error");
-    }
-    function success(pos) {
-        var crd = pos.coords;
-        var lat = crd.latitude.toString();
-        var long = crd.longitude.toString();
-        getCity(lat, long);
-        return;
-    }
-    function getCity(lat, long) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', "" + LOCATION_API_LINK + LOCATION_KEY + "\n        &lat=" + lat + "\n        &lng=" + long + "\n        &format=json", true);
-        xhr.send();
-        xhr.onreadystatechange = processRequest;
-        xhr.addEventListener("readystatechange", processRequest, false);
-        function processRequest(e) {
-            if (xhr.readyState == 4 && xhr.status == 200) {
-                var response = JSON.parse(xhr.responseText);
-                var city = response.address.city;
-                console.log(city);
-                return city;
-            }
-        }
-    }
-    navigator.geolocation.getCurrentPosition(success, error, options);
-    return "cityName";
+"use strict";
+exports.__esModule = true;
+exports.getWeatherData = void 0;
+function getWeatherData() {
 }
+exports.getWeatherData = getWeatherData;
--- weather-app/src/components/weatherAPI.ts
+++ weather-app/src/components/weatherAPI.ts
@@ -1,54 +1,105 @@
-const LOCATION_API_LINK = `https://us1.locationiq.com/v1/reverse.php?key=`;
-const LOCATION_KEY = `pk.3af66cb0b58e34d555ab8b604c40db2c`;
+const CITY = `tampa`;
+const WEATHER_KEY=`0c0482d33a1b1e06b025cb6d75a4a82f`
 
-function getLocation(): string {
-    var cityName: string;
-
-    const options = {
-        enableHighAccuracy: true,
-        timeout: 5000,
-        maximumAge: 0
-    };
-    function error(err: GeolocationPositionError): void{
-        console.warn(`error`);
-    }
-    function success(pos: GeolocationPosition) : void{
-        const crd = pos.coords;
-        const lat = crd.latitude.toString();
-        const long = crd.longitude.toString();
-
-        getCity(lat,long);
-
-        return;
-    }
-
-    function getCity(lat: string, long: string){
-        const xhr = new XMLHttpRequest();
-        
-        xhr.open('GET', `${LOCATION_API_LINK}${LOCATION_KEY}
-        &lat=${lat}
-        &lng=${long}
-        &format=json`,true);
-
-        xhr.send();
-        xhr.onreadystatechange = processRequest;
-        xhr.addEventListener("readystatechange", processRequest, false);
-
-        function processRequest(e: Event){
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                const response = JSON.parse(xhr.responseText);
-                const city = response.address.city;
-                console.log(city);
-                return city;
-            }
-        }
-        
-        
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
-    return `cityName`
+function getWeatherData(): Promise<Object>{
+    //Assuming your location is in tampa 
+   return pullAPIData().then((data) => {
+        const parsedData = weatherParser(data);
+        return data;
+    })
 }
 
-export {getLocation}
+async function pullAPIData(): Promise<Object>{
+    const url =`http://api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${WEATHER_KEY}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+function weatherParser(data: any){
+    let date=null;
+    const weatherStack = [];
+
+    for(let i=0; i < data.list.lenght; i++){
+        const newDate = data.list[i].dt_txt.split(" ")[0];
+        console.table(data.list[i]);
+
+        if(String(date) !== String(newDate)){
+            date = newDate;
+
+            const weatherWidget = new WeatherWidget(
+                data.list[i].main.temp,
+                data.list[i].weather[0].description,
+                data.list[i].pop,
+                data.list[i].main.humidity,
+                data.list[i].wind.speed,
+                date,
+                data.list[i].weather[0].icon,
+            ) 
+
+        }
+        weatherStack.push(WeatherWidget);
+        return weatherStack;
+    }
+}
+
+class WeatherWidget {
+    temperature: string;
+    description: string;
+    pop: string;
+    humidity: string;
+    wind: string;
+    date: string;
+    icon: string;
+    
+
+    constructor(
+        temperature: string,
+        description: string, 
+        pop: string,
+        humidity: string,
+        wind: string,
+        date: string,
+        icon: string,
+    ) {
+        this.temperature = temperature;
+        this.description = description;
+        this.pop = pop;
+        this.humidity = humidity;
+        this.wind = wind;
+        this.date = date;
+        this.icon = icon;
+    }
+
+    getTemp(){
+        return this.temperature;
+    }
+
+    getDescription(){
+        return this.description;
+    }
+
+    getPop(){
+        return this.pop;
+    }
+
+    getHumidity(){
+        return this.humidity;
+    }
+
+    getWind(){
+        return this.wind;
+    }
+
+    getDate(){
+        return this.date;
+    }
+
+    getIcon(){
+        return this.icon;
+    }
+}
+
+
+export {getWeatherData};
